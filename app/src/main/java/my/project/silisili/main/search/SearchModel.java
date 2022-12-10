7db6876048e27ed355c099da9bc416e611f7f997
LiuAlong <13573846313@163.com>
2022-12-10 22:44:30 +0800

    修复：搜索列表加载失败的问题

--- app/src/main/java/my/project/silisili/main/search/SearchModel.java
+++ app/src/main/java/my/project/silisili/main/search/SearchModel.java
@@ -2,11 +2,16 @@ package my.project.silisili.main.search;
 
 import android.util.Log;
 
+import androidx.core.net.UriCompat;
+
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Node;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Matcher;
@@ -22,9 +27,10 @@ import okhttp3.Response;
 
 public class SearchModel implements SearchContract.Model {
     private final static Pattern SEARCHID_PATTERN = Pattern.compile("searchid=(.*)");
+
     @Override
     public void getData(String title, String seaechID, int page, boolean isMain, SearchContract.LoadDataCallback callback) {
-        Log.e("page", page +"");
+        Log.e("page", page + "");
         if (page != 0) {
             // 如果不是第一页 则使用GET方式
             Log.e("分页GET", String.format(Api.SEARCH_GET_API, seaechID, page));
@@ -39,10 +45,16 @@ public class SearchModel implements SearchContract.Model {
                     getSearchData(response, isMain, callback);
                 }
             });
-        }else {
+        } else {
             // 第一次使用POST获取searchID
             Log.e("第一页POST", "GO" + title);
-            new HttpPost(Api.SEARCH_API,"wd=" + title , new Callback() {
+            String encodedUrl = title;
+            try {
+                encodedUrl = URLEncoder.encode(title, "UTF-8");
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            }
+            new HttpPost(Api.SEARCH_API, "wd=" + encodedUrl, new Callback() {
                 @Override
                 public void onFailure(Call call, IOException e) {
                     callback.error(isMain, e.getMessage());
@@ -58,59 +70,71 @@ public class SearchModel implements SearchContract.Model {
     }
 
     public void getSearchData(Response response, boolean isMain, SearchContract.LoadDataCallback callback) {
-        try{
+        try {
             Document body = Jsoup.parse(response.body().string());
             Elements animeList = body.getElementsByClass("post-list").select("div.entry-container");
             if (animeList.size() > 0) {
                 if (isMain) {
                     Elements pages = body.select("ul.list-page > li > a");
                     if (pages.size() > 0) {
-                        // 现在无法知道页码
-                        String page ="50";// pages.get(0).select("a").text().replaceAll(" ", "").replaceAll("下一页尾页", "");
+                        // 现在无法知道总页码
+                        String page = "50";// pages.get(0).select("a").text().replaceAll(" ", "").replaceAll("下一页尾页", "");
 //                        page = page.substring(page.length() - 1);
                         Matcher m = SEARCHID_PATTERN.matcher(pages.get(0).select("a").attr("href"));
                         String searchID = "";
                         while (m.find()) {
-                            searchID = m.group().replaceAll("searchid=","");
+                            searchID = m.group().replaceAll("searchid=", "");
                             break;
                         }
                         callback.searchID(searchID);
                         callback.pageCount(Integer.parseInt(page));
                     }
                 }
+                // 第一页肯定走这个方法
                 List<AnimeDescHeaderBean> list = new ArrayList<>();
                 for (int i = 0; i < animeList.size(); i++) {
                     AnimeDescHeaderBean bean = new AnimeDescHeaderBean();
-                    bean.setName(animeList.get(i).select("h3").text());
-                    bean.setImg(animeList.get(i).select("dt").select("img").attr("src").contains("http") ? animeList.get(i).select("dt").select("img").attr("src") : my.project.silisili.application.Silisili.DOMAIN + animeList.get(i).select("dt").select("img").attr("src"));
-                    bean.setUrl(animeList.get(i).select("h3").select("a").attr("href"));
-                    Elements label = animeList.get(i).getElementsByClass("d_label");
-                    for (int k = 0;k < label.size(); k++){
-                        String str = label.get(k).text();
-                        if (str.contains("地区"))
-                            bean.setRegion(str);
-                        else if (str.contains("年代"))
-                            bean.setYear(str);
-                        else if (str.contains("标签"))
-                            bean.setTag(str);
-                    }
-                    Elements p = animeList.get(i).select("p");
-                    for (int j = 0;j < p.size(); j++){
-                        String str = p.get(j).text();
-                        if (str.contains("看点"))
-                            bean.setShow(str);
-                        else if (str.contains("简介"))
-                            bean.setDesc(str);
-                        else if (str.contains("状态"))
-                            bean.setState(str);
-                    }
+                    bean.setName(animeList.get(i).select("div.entry-title > a").text());
+                    String imageSrc = animeList.get(i).select("div.search-image > a > img").attr("srcset");
+                    bean.setImg(imageSrc.contains("http") ? imageSrc : my.project.silisili.application.Silisili.DOMAIN + imageSrc);
+                    bean.setUrl(animeList.get(i).select("div.entry-title").select("a").attr("href"));
+                    //设置年代-其实是阅读量
+                    bean.setYear(animeList.get(i).select("time").text());
+                    Node metaNode = animeList.get(i).select("div.entry-meta").get(0);
+                    //设置标签
+                    bean.setTag(metaNode.childNode(0).toString().replaceAll("\n", ""));
+                    //设置简介
+                    bean.setDesc(animeList.get(i).select("div.entry-summary").select("p").text().trim());
+                    //设置状态
+                    bean.setState(metaNode.childNode(metaNode.childNodeSize() - 1).toString());
+                    // 这里只有一个标签字段，不再包含年份和地区等信息
+//                    Elements label = animeList.get(i).getElementsByClass("entry-meta");
+//                    for (int k = 0; k < label.size(); k++) {
+//                        String str = label.get(k).text();
+//                        if (str.contains("地区"))
+//                            bean.setRegion(str);
+//                        else if (str.contains("年代"))
+//                            bean.setYear(str);
+//                        else if (str.contains("标签"))
+//                            bean.setTag(str);
+//                    }
+//                    Elements p = animeList.get(i).select("p");
+//                    for (int j = 0; j < p.size(); j++) {
+//                        String str = p.get(j).text();
+//                        if (str.contains("看点"))
+//                            bean.setShow(str);
+//                        else if (str.contains("简介"))
+//                            bean.setDesc(str);
+//                        else if (str.contains("状态"))
+//                            bean.setState(str);
+//                    }
                     list.add(bean);
                 }
                 callback.success(isMain, list);
             } else {
                 callback.error(isMain, "没有搜索到相关信息");
             }
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
             callback.error(e.getMessage());
         }
