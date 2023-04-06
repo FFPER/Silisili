94c3a673790587190535bbe8f6c0d6e1d119da32
LiuAlong <13573846313@163.com>
2023-04-06 21:55:45 +0800

    开始改排行榜

--- app/src/main/java/my/project/silisili/adapter/RankingAdapter.java
+++ app/src/main/java/my/project/silisili/adapter/RankingAdapter.java
@@ -14,6 +14,7 @@ import my.project.silisili.bean.HomeWekBean;
 public class RankingAdapter extends BaseQuickAdapter<HomeWekBean, BaseViewHolder> {
     private Context context;
 
+    // TODO: 2023/4/6 先将颜色读取出来 
     public RankingAdapter(Context context, int layoutResId, @Nullable List<HomeWekBean> data) {
         super(layoutResId, data);
         this.context = context;
@@ -21,6 +22,6 @@ public class RankingAdapter extends BaseQuickAdapter<HomeWekBean, BaseViewHolder
 
     @Override
     protected void convert(BaseViewHolder helper, HomeWekBean item) {
-
+// TODO: 2023/4/6 修改不就文件 
     }
 }
